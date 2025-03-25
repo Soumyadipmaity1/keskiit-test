d48f85d456d5235932d5f1642171cb03ddb7317c
kaushik-2318 <kaushikverma321@gmail.com>
2025-03-25 15:04:37 +0530

    Achievemnt Page Setup and Navbar changed

---
+++ src/app/achievement/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/achievement/card";
+import Header from "@/components/achievement/header";
+
+export default function page() {
+  return (
+    <div className="bg-[#010B15] min-h-screen flex justify-center items-center">
+      <Header />
+      <Card/>
+    </div>
+  );
+}
