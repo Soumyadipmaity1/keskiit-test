763f21f707181c1cf9ad90788848381b2c5987d0
Soumyadip Maity <22053029@kiit.ac.in>
2025-03-29 00:46:24 +0530

    reduce header font size in Achievements section for improved responsiveness

--- src/components/achievement/header.tsx
+++ src/components/achievement/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div
         className={`${styles.background} w-full min-h-screen flex justify-center items-center py-20 flex-col`}
       >
-        <div className="font-extrabold text-6xl md:text-7xl leading-[102px] text-center">
+        <div className="font-extrabold text-5xl md:text-6xl leading-[102px] text-center">
           Achievements
         </div>
       </div>
