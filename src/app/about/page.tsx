eb4708e8d7f1d27ac41d8169de5bb44183bda88c
Soumyadip Maity <22053029@kiit.ac.in>
2025-03-29 00:43:10 +0530

    add About page component with styled layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-white flex justify-center text-5xl items-center h-screen bg-gradient-to-br from-[#0f172a] to-[#1e293b]'>
+      About US
+    </div>
+  )
+}
+
+export default page
